2f48ee439fc73ed44e3d98cb915d08e2bf4ad56e
Daler <dsarifkulo021@gmail.com>
2026-05-20 18:43:11 +0500

    fix: redirect from /ru to /

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/ru",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/ru/:path*",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+};
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 export default withNextIntl(nextConfig);
